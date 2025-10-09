8ce4037f76733cff50d67c730bb3c6e742226bc2
Lucas Costa <lucas.costa@simova.com.br>
2025-10-08 21:15:22 -0300

    first commit

---
+++ src/main/java/com/pdv/chaveiro/PdvChaveiroApplication.java
@@ -0,0 +1,13 @@
+package com.pdv.chaveiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvChaveiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvChaveiroApplication.class, args);
+	}
+
+}
